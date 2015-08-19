d746e9d030e1b2f4c512793894642498c04e27fe
Maxim Van de Wynckel <maxim@mvdw-software.com>
2015-08-19 20:55:46 +0200

    wrong import

--- src/main/java/be/maximvdw/placeholderapiexample/PlaceholderAPIExample.java
+++ src/main/java/be/maximvdw/placeholderapiexample/PlaceholderAPIExample.java
@@ -1,8 +1,8 @@
 package be.maximvdw.placeholderapiexample;
 
-import net.md_5.bungee.api.ChatColor;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
